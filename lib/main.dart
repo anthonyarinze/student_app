c5360d9cac618bc2c0ad4dd7e933e4ec18a53399
Anthony Arinze <anthonyarinze28@gmail.com>
2023-01-10 22:18:56 +0100

    Drawer & Sign Out

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:student_app/auth/login.dart';
 import 'package:student_app/pages/master.dart';
 import 'package:student_app/utils/firebase_options.dart';
 
@@ -22,7 +24,147 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Master(),
+      home: const MainPage(),
     );
   }
+}
+
+class MainPage extends StatelessWidget {
+  const MainPage({super.key});
+
+  @override
+  Widget build(BuildContext context) => SafeArea(
+        child: Scaffold(
+          drawer: Drawer(
+            elevation: 2.0,
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.only(
+                topRight: Radius.circular(20.0),
+                bottomRight: Radius.circular(20.0),
+              ),
+            ),
+            //ListView used to ensure user can scroll if there isn't enough vertical real estate.
+            child: ListView(
+              //Important: Remove any padding from the ListView.
+              padding: EdgeInsets.zero,
+              children: [
+                const SizedBox(height: 25),
+                DrawerHeader(
+                  decoration: const BoxDecoration(color: Colors.white),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey, width: 1),
+                      color: Colors.white,
+                      borderRadius: const BorderRadius.all(Radius.circular(40)),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(13, 20, 0, 20),
+                      child: Row(
+                        children: [
+                          const CircleAvatar(
+                            foregroundColor: Colors.white,
+                            backgroundColor: Color(0xFF26005f),
+                            radius: 40,
+                            child: Icon(
+                              Icons.person,
+                              size: 45,
+                            ),
+                          ),
+                          Flexible(
+                            child: Column(
+                              children: [
+                                const Padding(
+                                  padding: EdgeInsets.fromLTRB(0, 20, 80, 0),
+                                  child: Text(
+                                    "John Doe",
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold,
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text(
+                                    "john_doe@gmail.com",
+                                    overflow: TextOverflow.ellipsis,
+                                    style: TextStyle(
+                                      color: Colors.grey.shade700,
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.info,
+                    color: Colors.grey.shade700,
+                  ),
+                  title: const Text(
+                    "About",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 17,
+                    ),
+                  ),
+                  trailing:
+                      const Icon(Icons.arrow_forward_ios, color: Colors.grey),
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.lock,
+                    color: Colors.grey.shade700,
+                  ),
+                  title: const Text(
+                    "Change password",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 17,
+                    ),
+                  ),
+                  trailing:
+                      const Icon(Icons.arrow_forward_ios, color: Colors.grey),
+                ),
+                ListTile(
+                  onTap: () => FirebaseAuth.instance.signOut(),
+                  leading: Icon(
+                    Icons.logout_rounded,
+                    color: Colors.grey.shade700,
+                  ),
+                  title: const Text(
+                    "Sign Out",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 17,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          body: StreamBuilder<User?>(
+            stream: FirebaseAuth.instance.authStateChanges(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return const Master();
+              } else {
+                return const Login();
+              }
+            },
+          ),
+        ),
+      );
 }
