aa2f4a054c7a7a1ba7074f85d6d76dcfa5f3f4c7
Anthony Arinze <anthonyarinze28@gmail.com>
2023-01-12 14:11:34 +0100

    HomePage Progress

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -8,8 +8,47 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  String greeting() {
+    var hour = DateTime.now().hour;
+    if (hour < 12) {
+      return 'Morning';
+    }
+    if (hour < 17) {
+      return 'Afternoon';
+    }
+    return 'Evening';
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  const Text(
+                    'Good ',
+                    style: TextStyle(fontSize: 21),
+                  ),
+                  Text(
+                    greeting(),
+                    style: const TextStyle(fontSize: 21),
+                  ),
+                  const Text(
+                    ', Anthony',
+                    style: TextStyle(fontSize: 21, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
