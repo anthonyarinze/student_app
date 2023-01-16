83c06d7d8b08c101502b55018799fc13f7d7f114
Anthony Arinze <anthonyarinze28@gmail.com>
2023-01-16 03:10:20 +0100

    HomePage Progress

--- lib/utils/widgets.dart
+++ lib/utils/widgets.dart
@@ -62,57 +62,86 @@ class BuildClassWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Container(
-        height: 110,
-        width: MediaQuery.of(context).size.width,
-        decoration: BoxDecoration(
-          color: colors[0].withOpacity(0.6),
-          border: Border(
-            left: BorderSide(
-              color: colors[0],
-              width: 5,
+    return GestureDetector(
+      onTap: () => _dialogBuilder(context),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          height: 110,
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+            color: colors[0].withOpacity(0.6),
+            border: Border(
+              left: BorderSide(
+                color: colors[0],
+                width: 5,
+              ),
             ),
           ),
-        ),
-        child: Align(
-          alignment: Alignment.topLeft,
-          child: Padding(
-            padding: const EdgeInsets.fromLTRB(12.0, 8.0, 5.0, 8.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Law Class',
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: Colors.black.withOpacity(0.7),
-                    overflow: TextOverflow.ellipsis,
+          child: Align(
+            alignment: Alignment.topLeft,
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(12.0, 12.0, 5.0, 12.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Law Class',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.black.withOpacity(0.7),
+                      overflow: TextOverflow.ellipsis,
+                    ),
                   ),
-                ),
-                const Text(
-                  'Second Quiz',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                    overflow: TextOverflow.ellipsis,
+                  const Text(
+                    'Second Quiz',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      overflow: TextOverflow.ellipsis,
+                    ),
                   ),
-                ),
-                Text(
-                  '09:00 - 10:00',
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: Colors.black.withOpacity(0.7),
-                    overflow: TextOverflow.ellipsis,
+                  Text(
+                    '09:00 - 10:00',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.black.withOpacity(0.7),
+                      overflow: TextOverflow.ellipsis,
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
       ),
     );
   }
+}
+
+Future<void> _dialogBuilder(BuildContext context) {
+  return showDialog<void>(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: const Text('Basic dialog title'),
+        content: const Text('A dialog is a type of modal window that\n'
+            'appears in front of app content to\n'
+            'provide critical information, or prompt\n'
+            'for a decision to be made.'),
+        actions: <Widget>[
+          TextButton(
+            style: TextButton.styleFrom(
+              textStyle: Theme.of(context).textTheme.labelLarge,
+            ),
+            child: const Text('Close'),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      );
+    },
+  );
 }
